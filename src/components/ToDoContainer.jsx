364d36a9a0d19f1ebaa2f3bda60cd8b49560993b
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-22 18:50:53 +0800

    move to do list and the corresponding function to container

--- src/components/ToDoContainer.jsx
+++ src/components/ToDoContainer.jsx
@@ -1,12 +1,46 @@
 import React, { Component } from "react";
+import update from "immutability-helper";
+import ToDoListAPI from "../apis/ToDoListAPI";
 import TaskForm from "./TaskForm";
 import ToDoList from "./ToDoList";
 
 export default class ToDoContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { toDoList: [] };
+  }
+
+  componentDidMount() {
+    ToDoListAPI.getToDoList().then((response) => {
+      const toDoList = response.data;
+      this.setState({ toDoList });
+    });
+  }
+
+  markAsCompleted = (id) => {
+    const { toDoList } = this.state;
+    const index = toDoList.findIndex((task) => task.id === id);
+    const updatedToDoList = update(toDoList, {
+      [index]: { $merge: { status: !toDoList[index].status } },
+    });
+    this.setState({ toDoList: updatedToDoList });
+  };
+
+  removeTask = (id) => {
+    const { toDoList } = this.state;
+    const index = toDoList.findIndex((task) => task.id === id);
+    toDoList.splice(index, 1);
+    this.setState({ toDoList });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <ToDoList />
+        <ToDoList
+          toDoList={this.state.toDoList}
+          markAsCompleted={this.markAsCompleted}
+          removeTask={this.removeTask}
+        />
         <TaskForm />
       </React.Fragment>
     );
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,41 +1,16 @@
 import React, { Component } from "react";
-import ToDoListAPI from "../apis/ToDoListAPI";
 import { Alert } from "react-bootstrap";
 import "./ToDoList.css";
-import update from "immutability-helper";
 
 export default class ToDoList extends Component {
   constructor(props) {
     super(props);
-    this.state = { toDoList: [] };
   }
-
-  componentDidMount() {
-    ToDoListAPI.getToDoList().then((response) => {
-      const toDoList = response.data;
-      this.setState({ toDoList });
-    });
-  }
-
-  markAsCompleted = (id) => {
-    const { toDoList } = this.state;
-    const index = toDoList.findIndex((task) => task.id === id);
-    const updatedToDoList = update(toDoList, {
-      [index]: { $merge: { status: !toDoList[index].status } },
-    });
-    this.setState({ toDoList: updatedToDoList });
-  };
-
-  removeTask = (id) => {
-    const { toDoList } = this.state;
-    const index = toDoList.findIndex((task) => task.id === id);
-    toDoList.splice(index, 1);
-    this.setState({ toDoList });
-  };
 
   renderToDoList = () => {
-    console.log("-----renderToDoList------");
-    return this.state.toDoList.map((task, index) => {
+    const { toDoList, removeTask, markAsCompleted } = this.props;
+
+    return toDoList.map((task, index) => {
       const content = task.status ? (
         <span>{task.content}</span>
       ) : (
@@ -45,10 +20,10 @@ export default class ToDoList extends Component {
         <Alert
           key={index}
           variant="success"
-          onClose={() => this.removeTask(task.id)}
+          onClose={() => removeTask(task.id)}
           dismissible
         >
-          <span onClick={() => this.markAsCompleted(task.id)}>{content}</span>
+          <span onClick={() => markAsCompleted(task.id)}>{content}</span>
         </Alert>
       );
     });
