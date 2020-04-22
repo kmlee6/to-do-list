5f5ee407825e966714c1ff5121031e15a077355c
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-22 23:56:37 +0800

    add put API call for update task

--- src/apis/ToDoListAPI.js
+++ src/apis/ToDoListAPI.js
@@ -5,6 +5,10 @@ class ToDoListAPI {
   static getToDoList() {
     return axios.get(TO_DO_LIST_API_BASE_URL);
   }
+
+  static updateToDoTask(id, data) {
+    return axios.put(TO_DO_LIST_API_BASE_URL + "/" + id, data);
+  }
 }
 
 export default ToDoListAPI;
--- src/components/ToDoContainer.jsx
+++ src/components/ToDoContainer.jsx
@@ -27,13 +27,22 @@ export default class ToDoContainer extends Component {
     this.setState({ toDoList: updatedToDoList });
   };
 
+  updateToDoList = (id, updatedTask) => {
+    ToDoListAPI.updateToDoTask(id, updatedTask).then((response) => {
+      console.log(response);
+    });
+  };
+
   markAsCompleted = (id) => {
     const { toDoList } = this.state;
     const index = toDoList.findIndex((task) => task.id === id);
     const updatedToDoList = update(toDoList, {
       [index]: { $merge: { status: !toDoList[index].status } },
     });
-    this.setState({ toDoList: updatedToDoList });
+    this.setState(
+      { toDoList: updatedToDoList },
+      this.updateToDoList(id, updatedToDoList[index])
+    );
   };
 
   removeTask = (id) => {
